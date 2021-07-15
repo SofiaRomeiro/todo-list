4e42efa12b1ca768e27c1b1ea6875c2c00b94f08
Sofia Romeiro <sofia.romeiro@contisystems.pt>
2021-07-15 13:10:25 +0100

    Delete Task feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Divider from "@material-ui/core/Divider";
 import Header from "./components/Header";
 import NewTask from "./components/CreateNewTask";
@@ -10,13 +10,43 @@ import Footer from "./components/Footer";
 
 const App = () => {
   const [showNewTaskForm, setShowNewTaskForm] = useState(true);
+  const [tasksList, setTasksList] = useState([
+    {
+      id: 1,
+      description: "Levar o gato a passear",
+      time: "9:00",
+      priority: "media"
+    },
+    {
+      id: 2,
+      description: "inscrever no exame de karate",
+      time: "20:00",
+      priority: "alta"
+    },
+    {
+      id: 3,
+      description: "jogar valorant",
+      time: "23:00",
+      priority: "baixa"
+    },
+    {
+      id: 4,
+      description: "comprar bilhete para Leiria",
+      time: "15:00",
+      priority: "alta"
+    }
+  ]);
+
+  const deleteTask = (id) => {
+    setTasksList(tasksList.filter((task) => task.id !== id));
+  };
 
   return (
     <Router>
       <div className="container">
         <Header />
 
-        <Tasks />
+        <Tasks tasks={tasksList} onDelete={deleteTask} />
 
         <Divider
           variant="middle"
